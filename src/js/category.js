172ae94c239f64044ac80e8259d07ace438d31dd
an donghyun <ax34554@gmail.com>
2023-04-20 22:07:26 +0900

    fix: add 상품등록 modal, 상품수정 modal

--- src/js/category.js
+++ src/js/category.js
@@ -1,4 +1,7 @@
 const adminNavigation = document.querySelectorAll(".header-nav-list ul li a");
+const fixModal = document.querySelector(".fix_modal");
+const fixButton = document.querySelectorAll(".item-fix-button");
+const closeFixButton = document.querySelector('.fix-modal-close');
 
 const getType = (clicks) => {
   return clicks.dataset.type;
@@ -12,3 +15,37 @@ const displayToggle = (e) => {
 adminNavigation.forEach((nav) => {
   nav.addEventListener("click", displayToggle);
 });
+
+
+const openFixModal = () => {
+  fixModal.classList.add("on") || updateModal.classList.remove("on");
+}
+
+const closeFixModal = () => {
+  fixModal.classList.remove("on") || updateModal.classList.remove("on");
+}
+
+fixButton.forEach(button => {
+  button.addEventListener('click', openFixModal);
+});
+
+closeFixButton.addEventListener('click', closeFixModal);
+
+
+const updateModal = document.querySelector(".update_modal");
+const addButton = document.querySelector(".add-item");
+const closeUpdateButton = document.querySelector('.update-modal-close');
+
+const openUpdateModal = () => {
+  updateModal.classList.add('on');
+}
+
+const closeUpdateModal = () => {
+  updateModal.classList.remove('on');
+}
+
+addButton.addEventListener('click', openUpdateModal);
+closeUpdateButton.addEventListener('click', closeUpdateModal);
+
+
+
