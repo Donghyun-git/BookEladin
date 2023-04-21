1989356df04b52252a165da4e6fabeb3211401fc
Seongkyoung Kwon <seongkyoungkwon@Seongkyoungui-MacBookAir.local>
2023-04-21 15:36:05 +0900

    fix 주석처리

--- src/js/order.js
+++ src/js/order.js
@@ -1,6 +1,8 @@
 const prices = document.querySelectorAll(".price");
 const orderPrice = document.querySelector(".order-price");
 const chevron = document.querySelectorAll(".fa-chevron-down");
+// const purchaseBtn = document.querySelector(".purchase-btn");
+// const essentialInput = document.querySelectorAll(".essential");
 
 let totalPrice = 0;
 
@@ -13,29 +15,27 @@ let result = totalPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 orderPrice.innerHTML = result;
 
-chevron[0].addEventListener("click", () => {
-    let box = chevron[0].parentElement.nextSibling.nextSibling;
-    if (box.style.display == "none") {
-        box.style.display = "block";
-    } else {
-        box.style.display = "none";
-    }
-});
-
-chevron[1].addEventListener("click", () => {
-    let box = chevron[1].parentElement.nextSibling.nextSibling;
-    if (box.style.display == "none") {
-        box.style.display = "flex";
-    } else {
-        box.style.display = "none";
-    }
-});
-
-chevron[2].addEventListener("click", () => {
-    let box = chevron[2].parentElement.nextSibling.nextSibling;
-    if (box.style.display == "none") {
-        box.style.display = "grid";
-    } else {
-        box.style.display = "none";
-    }
-});
+// chevron[0].addEventListener("click", () => {
+//     let box = chevron[0].parentElement.nextSibling.nextSibling;
+//     if (box.style.display == "none") {
+//         box.style.display = "block";
+//     } else {
+//         box.style.display = "none";
+//     }
+// });
+// chevron[1].addEventListener("click", () => {
+//     let box = chevron[1].parentElement.nextSibling.nextSibling;
+//     if (box.style.display == "none") {
+//         box.style.display = "flex";
+//     } else {
+//         box.style.display = "none";
+//     }
+// });
+// chevron[2].addEventListener("click", () => {
+//     let box = chevron[2].parentElement.nextSibling.nextSibling;
+//     if (box.style.display == "none") {
+//         box.style.display = "grid";
+//     } else {
+//         box.style.display = "none";
+//     }
+// });
