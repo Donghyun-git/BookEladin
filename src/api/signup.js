6a521a610c86141cb7a02ace422590f84dfe9fbd
an donghyun <ax34554@gmail.com>
2023-04-23 05:25:09 +0900

    refactor: 전역변수 이벤트핸들러 지역변수로 수정

--- src/api/signup.js
+++ src/api/signup.js
@@ -1,27 +1,27 @@
-const id = document.querySelector("#id");
-const password = document.querySelector("#password");
-const userName = document.querySelector("#name");
-const email = document.querySelector("#email");
-
 const signupButton = document.querySelector("#signup-btn");
 
 signupButton.addEventListener("click", async () => {
-  const uri = "http://localhost:5500/auth/signup";
-  const userData = {
-    userId: id.value,
-    password: password.value,
-    userName: userName.value,
-    email: email.value,
-  };
+    const id = document.querySelector("#id").value;
+    const password = document.querySelector("#password").value;
+    const userName = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const uri = "http://localhost:5500/auth/signup";
+    const userData = {
+        userId: id,
+        password: password,
+        userName: userName,
+        email: email,
+    };
 
-  await axios.post(uri, userData)
-    .then((res) => {
-      console.log(res);
-      window.alert(`${res.data.message}`);
-      window.location.href = "./login.html";
-    })
-    .catch((error) => {
-      console.error(error);
-      window.alert(`${error.response.data.message}`);
-    });
+    await axios
+        .post(uri, userData)
+        .then((res) => {
+            console.log(res);
+            window.alert(`${res.data.message}`);
+            window.location.href = "./login.html";
+        })
+        .catch((error) => {
+            console.error(error);
+            window.alert(`${error.response.data.message}`);
+        });
 });
