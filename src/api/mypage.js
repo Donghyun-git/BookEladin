c10ab9b5d7b3b49e5a3d692016797b7a05b7e60d
an donghyun <ax34554@gmail.com>
2023-04-23 05:22:42 +0900

    fix: localStorage property name token -> accessToken

--- src/api/mypage.js
+++ src/api/mypage.js
@@ -3,21 +3,14 @@
 const myPage = document.querySelector(".header-info-my");
 
 myPage.addEventListener("click", async (e) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("accessToken");
 
     if (!token) {
         e.preventDefault();
         window.alert("로그인 후에 이용해주세요!");
-        window.location.href = "/login";
+        window.location.href = "./login.html";
     } else {
-        const header = {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        };
-        const myResponse = await axios.get("/mypage", header);
-        const myData = myResponse.data;
-
-        localStorage.setItem("mypage", JSON.stringify(myData)); //마이페이지 버튼을 눌러 가져온 데이터를 로컬스토리지 저장. 후에 마이페이지 렌더링할 때 사용.
+        //마이페이지 렌더링... 진행중..
+        window.alert('4.23 일어나서 할예정');
     }
 });
