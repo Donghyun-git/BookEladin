91c8d11aa5d4c68d1a92ed2d5b939fb132521fb3
an donghyun <ax34554@gmail.com>
2023-04-24 21:19:59 +0900

    style: code prettier

--- src/js/mypageRender.js
+++ src/js/mypageRender.js
@@ -1,6 +1,5 @@
 //어차피 마이페이지 접근은 토큰이 있을 때만 접근 가능함.
 const myData = JSON.parse(localStorage.getItem('myData'));
-
 const userName = document.querySelector('.user-name');
 const userEmail = document.querySelector('.email');
 
