06c2cbf9f0bf211c5d54aa356994ceb21fabafc1
bible <mongry999@gmail.com>
2023-04-24 16:01:14 +0900

    feat:Update main product page layout

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ const productsArea = document.querySelector(".products-list");
 const productsList = JSON.parse(localStorage.getItem("productsList"));
 console.log(productsList);
 
-let productsHtml;
+//초기값을 ""으로 할당해주지 않으면 undefined로 초기화되어 출력됨!
+let productsHtml = "";
 
-productsList.forEach((products) => {
-    const { author, category, introduction, price, productId, title } = products;
-    if(products) {
+//slice로 9개만 가져옴
+productsList.slice(0, 9).forEach((products) => {
+    const { author, category, introduction, price, productId, title, imgUrl } =
+        products;
+
+    shortIntro = introduction.slice(0, 30) + "...";
+    if (products) {
         productsHtml += `
-        <div class="products-list-info" style="width: 200px; height: 300px; border: 1px solid black; display: inline-block;">
-            <p>${category}</p>
-            <p>${productId}</p>
-            <p>${title}</p>
-            <p>${author}</p>
-            <p>${introduction}</p>
-            <p>${price}</p>
+        <div class="products-list-info">
+            <img class="product-cover" src="${imgUrl}" alt="example_cover"/>
+            <p class= "title">${title}</p>
+            <p class="author">${author}</p>
+            <p class ="introduction">${shortIntro}</p>
+            <p class="price">${price}</p>
         </div>
     `;
     }
