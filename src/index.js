7d0f360f8ab4ec47acd8527f5cfb6e7cb4b12a1c
bible <mongry999@gmail.com>
2023-04-25 16:18:08 +0900

    fix: update product slider, add product quatity, fix price currency in index.js, index.html. index.css

--- src/index.js
+++ src/index.js
@@ -1,52 +1,42 @@
-const bestSellerArea = document.querySelector('.best-products');
-const newArea = document.querySelector('.new-products');
-const recommendArea = document.querySelector('.recommend-products');
-import { fetchProducts } from './api/getProducts.js';
+const bestSellerArea = document.querySelector(".best-products");
+const newArea = document.querySelector(".new-products");
+const recommendArea = document.querySelector(".recommend-products");
+import { fetchProducts } from "./api/getProducts.js";
 
 const productsList = await fetchProducts();
 console.log(productsList);
 
 //초기값을 ""으로 할당해주지 않으면 undefined로 초기화되어 출력됨!
-let bestProductsHtml = '';
-let RecommendProductsHtml = '';
-let newProductsHtml = '';
+let bestProductsHtml = "";
+let RecommendProductsHtml = "";
+let newProductsHtml = "";
 
-//slice로 10개만 가져옴
+//slice로 상품 30개씩 가져옴
 let bestproducts = productsList
     .filter((product) => (product.bestSeller = true))
-    .slice(0, 10);
+    .slice(0, 30);
 let newProducts = productsList
     .filter((product) => (product.newBook = true))
-    .slice(10, 20);
+    .slice(10, 40);
 let RecommendProducts = productsList
     .filter((product) => (product.recommend = true))
-    .slice(20, 30);
-
+    .slice(20, 50);
+//html 상품 넣기
 function addProductList(productsType, typeArea) {
-    let newHtml = '';
+    let newHtml = "";
     productsType.forEach((products) => {
-        const {
-            author,
-            category,
-            introduction,
-            price,
-            title,
-            imgUrl,
-            bestSeller,
-            newBook,
-            recommend,
-        } = products;
+        const { author, introduction, price, title, imgUrl } = products;
+
+        const formattedPrice = price.toLocaleString() + "원";
 
         if (products) {
-            // shortIntro = introduction.slice(0, 30) + "...";
-
             newHtml += `
         <li class="products-list-info">
             <img class="product-cover" src="${imgUrl}" alt="example_cover"/>
             <p class= "title">${title}</p>
             <p class="author">${author}</p>
             <p class ="introduction">${introduction}</p>
-            <p class="price">${price}</p>
+            <p class="price">${formattedPrice}</p>
         </li>
     `;
         }
@@ -54,34 +44,51 @@ function addProductList(productsType, typeArea) {
     typeArea.innerHTML = newHtml;
 }
 
-// bestSellerArea.innerHTML = bestProductsHtml;
 addProductList(bestproducts, bestSellerArea);
 addProductList(newProducts, newArea);
 addProductList(RecommendProducts, recommendArea);
 
+//상품 슬라이더 시작
 let currentIdx = [0, 0, 0];
 
-const chevronRightBtn = document.querySelectorAll('.fa-chevron-circle-right');
-chevronRightBtn.forEach((button, idx) => {
-    button.addEventListener('click', function () {
-        let slides = button.parentElement.querySelector('.slides');
+const chevronRightBtn = document.querySelectorAll(".fa-chevron-circle-right");
+const chevronLeftBtn = document.querySelectorAll(".fa-chevron-circle-left");
 
+//오른쪽 버튼 효과
+
+chevronRightBtn.forEach((button, idx) => {
+    button.addEventListener("click", function () {
+        let slides = button.parentElement.querySelector(".slides");
+        //버튼 나타나고 사라지는 효과
         if (currentIdx[idx] === 0) {
+            chevronLeftBtn[idx].style.opacity = 1;
+        } else if (currentIdx[idx] === 4) {
+            button.style.opacity = 0;
+        }
+        //슬라이딩 되는 부분
+        if (currentIdx[idx] < 5) {
             console.log(currentIdx);
-            slides.style.left = -950 + 'px';
+            slides.style.left = -(currentIdx[idx] + 1) * 950 + "px";
             currentIdx[idx] += 1;
         }
     });
 });
 
-const chevronLeftBtn = document.querySelectorAll('.fa-chevron-circle-left');
-chevronLeftBtn.forEach((button, idx) => {
-    button.addEventListener('click', function () {
-        let slides = button.parentElement.querySelector('.slides');
+//왼쪽 버튼 효과
 
+chevronLeftBtn.forEach((button, idx) => {
+    button.addEventListener("click", function () {
+        let slides = button.parentElement.querySelector(".slides");
+        //버튼 나타나고 사라지는 효과
         if (currentIdx[idx] === 1) {
+            button.style.opacity = 0;
+        } else if (currentIdx[idx] === 5) {
+            chevronRightBtn[idx].style.opacity = 1;
+        }
+        //슬라이딩 되는 부분
+        if (currentIdx[idx] > 0) {
             console.log(currentIdx);
-            slides.style.left = 0;
+            slides.style.left = -(currentIdx[idx] - 1) * 950 + "px";
             currentIdx[idx] -= 1;
         }
     });
