95cec9d9273c6ff3f504b70227748c6600753bc3
bible <mongry999@gmail.com>
2023-04-27 05:44:59 +0900

    fix: add phone number input validation in order.js

--- src/js/order.js
+++ src/js/order.js
@@ -1,33 +1,33 @@
-import API from '../api/rest.js';
-import IDB from './indexedDB.js';
+import API from "../api/rest.js";
+import IDB from "./indexedDB.js";
 
-const chevron = document.querySelectorAll('.fa-chevron-down');
-const form = document.getElementById('form');
-const esseatialInput = document.querySelectorAll('.essential');
-const List = document.querySelector('.product-list');
+const chevron = document.querySelectorAll(".fa-chevron-down");
+const form = document.getElementById("form");
+const esseatialInput = document.querySelectorAll(".essential");
+const List = document.querySelector(".product-list");
 
-chevron[0].addEventListener('click', () => {
+chevron[0].addEventListener("click", () => {
     let box = chevron[0].parentElement.nextSibling.nextSibling;
-    if (box.style.display == 'none') {
-        box.style.display = 'block';
+    if (box.style.display == "none") {
+        box.style.display = "block";
     } else {
-        box.style.display = 'none';
+        box.style.display = "none";
     }
 });
-chevron[1].addEventListener('click', () => {
+chevron[1].addEventListener("click", () => {
     let box = chevron[1].parentElement.nextSibling.nextSibling;
-    if (box.style.display == 'none') {
-        box.style.display = 'flex';
+    if (box.style.display == "none") {
+        box.style.display = "flex";
     } else {
-        box.style.display = 'none';
+        box.style.display = "none";
     }
 });
-chevron[2].addEventListener('click', () => {
+chevron[2].addEventListener("click", () => {
     let box = chevron[2].parentElement.nextSibling.nextSibling;
-    if (box.style.display == 'none') {
-        box.style.display = 'grid';
+    if (box.style.display == "none") {
+        box.style.display = "grid";
     } else {
-        box.style.display = 'none';
+        box.style.display = "none";
     }
 });
 
@@ -46,7 +46,7 @@ class OrderList {
         await this.setOrder();
         const orderList = this.order;
 
-        let template = '';
+        let template = "";
         orderList.map((order) => {
             template += `
                 <section class="product-detail">
@@ -70,12 +70,12 @@ class OrderList {
 
     async totalPrice() {
         let total = 0;
-        const orderPrice = document.querySelector('.order-price');
+        const orderPrice = document.querySelector(".order-price");
         await this.order.map((o) => {
             total += Number(o.price);
         });
 
-        orderPrice.innerText = total.toLocaleString() + '원';
+        orderPrice.innerText = total.toLocaleString() + "원";
     }
 
     async render() {
@@ -102,28 +102,31 @@ class OrderForm {
 
     validate() {
         this.setState();
-        const checkBtn = document.getElementById('check-btn');
+        const checkBtn = document.getElementById("check-btn");
 
         for (let idx = 0; idx < this.state.length - 1; idx++) {
-            if (this.state[idx] === '') {
-                alert('필수 입력 사항을 모두 작성해주세요.');
+            if (this.state[idx] === "") {
+                alert("필수 입력 사항을 모두 작성해주세요.");
 
-                throw new Error('필수 입력 사항을 모두 작성해주세요.');
+                throw new Error("필수 입력 사항을 모두 작성해주세요.");
             }
         }
+        const regul = /^01\d{1}-\d{4}-\d{4}$/;
+        if (!regul.test(this.state[4])) {
+            alert("휴대전화는 010-0000-0000형식으로 입력해주세요.");
+            throw new Error("휴대전화는 010-0000-0000 형식으로 입력해주세요.");
+        } else if (checkBtn.checked === false) {
+            alert("구매 동의 항목에 체크해주세요.");
 
-        if (checkBtn.checked === false) {
-            alert('구매 동의 항목에 체크해주세요.');
-
-            throw new Error('구매 동의 항목에 체크해주세요');
+            throw new Error("구매 동의 항목에 체크해주세요");
         }
 
         return true;
     }
 
     addEvent() {
-        this.form.addEventListener('click', (e) => {
-            if (e.target.classList.contains('purchase-btn')) {
+        this.form.addEventListener("click", (e) => {
+            if (e.target.classList.contains("purchase-btn")) {
                 try {
                     this.validate();
                     this.postDeliveryInfo();
@@ -158,9 +161,9 @@ class OrderForm {
 
     async postDeliveryInfo() {
         const items = await this.orderIDB();
-        const userData = JSON.parse(localStorage.getItem('userData'));
-        const uuid = localStorage.getItem('uuid');
-        const accessToken = localStorage.getItem('accessToken');
+        const userData = JSON.parse(localStorage.getItem("userData"));
+        const uuid = localStorage.getItem("uuid");
+        const accessToken = localStorage.getItem("accessToken");
 
         const userId = userData ? userData.userId : uuid;
         console.log(userId);
@@ -181,23 +184,24 @@ class OrderForm {
         console.log(data);
         const header = accessToken
             ? {
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-                withCredentials: true,
-            }
+                  headers: {
+                      Authorization: `Bearer ${accessToken}`,
+                  },
+                  withCredentials: true,
+              }
             : { withCredentials: true };
-        
-            const url = accessToken ? 'http://localhost:5500/orders/user'
-            : 'http://localhost:5500/orders/nonmember';
+
+        const url = accessToken
+            ? "http://localhost:5500/orders/user"
+            : "http://localhost:5500/orders/nonmember";
         try {
             const response = await axios.post(url, data, header);
 
             console.log(response.data.data);
-            window.alert('결제가 완료되었습니다.');
-            localStorage.setItem('order', JSON.stringify(response.data.data));
+            window.alert("결제가 완료되었습니다.");
+            localStorage.setItem("order", JSON.stringify(response.data.data));
             this.deleteIDB();
-            location.href = 'order_ok.html';
+            location.href = "order_ok.html";
         } catch (err) {
             console.log(err);
         }
