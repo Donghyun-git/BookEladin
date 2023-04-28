1309eb0f6f150a855edae29d4d5f7e3f101e3f47
an donghyun <ax34554@gmail.com>
2023-04-28 17:25:23 +0900

    fix:mypage render

--- src/api/mypage.js
+++ src/api/mypage.js
@@ -1,38 +1,49 @@
 // mypage 로고 누를 시 마이페이지 출력하기 위한 로직
-const myPage = document.querySelector('.header-info-my');
+const myPage = document.querySelector(".header-info-my");
 
 const getUserInfo = async (e) => {
     e.preventDefault();
-    const accessToken = localStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
 
     if (!accessToken) {
         e.preventDefault();
-        window.alert('로그인 후에 이용해주세요!');
-        window.location.href = './login.html';
+        window.alert("로그인 후에 이용해주세요!");
+        window.location.href = "./login.html";
     } else {
         try {
-            const userData = JSON.parse(localStorage.getItem('userData'));
+            const userData = JSON.parse(localStorage.getItem("userData"));
             const userId = userData.userId;
             const header = accessToken
                 ? {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                    withCredentials: true,
-                }
+                      headers: {
+                          Authorization: `Bearer ${accessToken}`,
+                      },
+                      withCredentials: true,
+                  }
                 : { withCredentials: true };
             const uri = `http://localhost:5500/auth/users/${userId}`;
 
             const myPageResponse = await axios.get(uri, header);
             const myPageData = myPageResponse.data;
 
-            localStorage.setItem('myData', JSON.stringify(myPageData));
-            window.location.href = './mypage.html';
+            localStorage.setItem("myData", JSON.stringify(myPageData));
+            window.location.href = "./mypage.html";
         } catch (err) {
             console.log(err);
         }
     }
 };
+
+myPage.addEventListener("click", getUserInfo);
+
+
+/* 비회원, 게스트 마이페이지 링크 x */
+
+const blockMoveToMypqge = async () => {
+    if(localStorage.getItem('uuid') || !localStorage.getItem('accessToken')){
+        window.alert('비회원이나 게스트는 이용하실 수 없습니다!')
+    }
+}
 
 
 
--- src/js/mypageRender.js
+++ src/js/mypageRender.js
@@ -1,19 +1,21 @@
-//어차피 마이페이지 접근은 토큰이 있을 때만 접근 가능함.
 window.addEventListener("load", async () => {
-   const myData = await JSON.parse(localStorage.getItem("myData"));
-   const userData = await JSON.parse(localStorage.getItem("userData"));
-   const userName = document.querySelector(".user-name");
-   const userEmail = document.querySelector(".email");
+    const myDataJson = localStorage.getItem("myData");
+    const userDataJson = localStorage.getItem("userData");
 
-   if (
-       myData.data.userName !== userData.userName ||
-       myData.data.email !== userData.email
-   ) {
-       userName.innerHTML = myData.data.userName;
-       userEmail.innerHTML = myData.data.email;
-   } else {
-       userName.innerHTML = userData.userName;
-       userEmail.innerHTML = userData.email;
-   }
+    const myData = myDataJson && JSON.parse(myDataJson);
+    const userData = userDataJson && JSON.parse(userDataJson);
+
+    const userName = document.querySelector(".user-name");
+    const userEmail = document.querySelector(".email");
+
+    if (
+        (myData && userData && myData.data.userName !== userData.userName) ||
+        myData.data.email !== userData.email
+    ) {
+        userName.innerHTML = myData.data.userName;
+        userEmail.innerHTML = myData.data.email;
+    } else if (userData) {
+        userName.innerHTML = userData.userName;
+        userEmail.innerHTML = userData.email;
+    }
 });
-
