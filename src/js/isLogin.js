6975f93c700c07c9540434a38e71c7a842fe6fb4
an donghyun <ax34554@gmail.com>
2023-04-28 18:04:00 +0900

    feat: guest login sesseion

--- src/js/isLogin.js
+++ src/js/isLogin.js
@@ -6,30 +6,44 @@
         if (localStorage.getItem("myData")) {
             const myData = JSON.parse(localStorage.getItem("myData"));
             fixHeader.innerHTML = `
-        <li class="header-top-item">
-            <a class="join"
-                >${myData.data.userName} 님 환영합니다!</a
-            >
-        </li>
-        <li class="header-top-item">
-            <a id="logout-button" class="login"
-                >로그아웃</a
-            >
-        </li>
-    `;
+            <li class="header-top-item">
+                <a class="join"
+                    >${myData.data.userName} 님 환영합니다!</a
+                >
+            </li>
+            <li class="header-top-item">
+                <a id="logout-button" class="login"
+                    >로그아웃</a
+                >
+            </li>
+        `;
         } else {
             fixHeader.innerHTML = `
-        <li class="header-top-item">
-            <a class="join"
-                >${userData.userName} 님 환영합니다!</a
-            >
-        </li>
-        <li class="header-top-item">
-            <a id="logout-button" class="login"
-                >로그아웃</a
-            >
-        </li>
-    `;
+            <li class="header-top-item">
+                <a class="join"
+                    >${userData.userName} 님 환영합니다!</a
+                >
+            </li>
+            <li class="header-top-item">
+                <a id="logout-button" class="login"
+                    >로그아웃</a
+                >
+            </li>
+        `;
         }
+    } else if (localStorage.getItem("uuid")) {
+        let fixHeader = document.querySelector(".header-top-list");
+        fixHeader.innerHTML = `
+            <li class="header-top-item">
+                <a class="join"
+                    >비회원으로 로그인 중입니다.</a
+                >
+            </li>
+            <li class="header-top-item">
+                <a id="logout-button" class="login"
+                    >로그아웃</a
+                >
+            </li>
+        `;
     }
 
